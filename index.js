a1f54ff50ea41e6354ec0bbb756444174c9ee200
Alex <aamorgun2003@gmail.com>
2023-12-30 13:50:21 -0700

    Adjust digit function and DOM variables
    Calculator always displays a zero. I didn't want the user
    to be able to click an operator before a number was selected. Digits function now checks for a number of conditions before it replaces the default value: zero. So now when the user clicks an operator first, it will apply the operation to the default value: zero.
    
    Another function checkForOperators was added because I was using the logic in multiple functions.

--- index.js
+++ index.js
@@ -1,70 +1,78 @@
 const calScript = document.querySelector("#calScript");
 const digits = document.querySelectorAll(".digit");
 const operators = document.querySelectorAll(".operator");
-const calcDisplay = document.querySelector(".display").textContent;
+const calcDisplay = document.querySelector(".display");
 const calcClear = document.querySelector(".clear");
 const equalSign = document.querySelector("#equals");
-//let calcDisplayValue = "";
-
 
 equalSign.addEventListener("click", () => {
-  operate(calcDisplayValue);
+  operate(calcDisplay.textContent);
 });
 
 calScript.addEventListener("load", () => {
-  calcDisplay.setAttribute("value", "0");
+  calcDisplay.textContent = 0;
 });
 
 calcClear.addEventListener("click", () => {
-  calcDisplay.textContent = "";
-  calcDisplayValue = "";
+  calcDisplay.textContent = 0;
   operators.forEach((item) => item.removeAttribute("disabled"));
 });
 
 digits.forEach((item) => {
   item.addEventListener("click", () => {
-    console.log(item)
-    console.log(calcDisplay)
-    calcDisplay = calcDisplay + item.value;
-    console.log(calcDisplay)
+    // console.log(calcDisplay.textContent.includes("0"))
+    // console.log(!checkForOperators(calcDisplay))
+    // console.log(calcDisplay.textContent.length <=  1)
+    if (calcDisplay.textContent.includes("0") && !checkForOperators(calcDisplay) && calcDisplay.textContent.length <= 1) {
+      let replaced = calcDisplay.textContent.replace("0", item.value);
+      calcDisplay.textContent = replaced;
+    } else {
+      calcDisplay.textContent += item.value
+    }
   });
 });
 
 operators.forEach((item) => {
-  console.log(item)
   item.addEventListener("click", () => {
-    if (
-      calcDisplayValue.includes("+") ||
-      calcDisplayValue.includes("-") ||
-      calcDisplayValue.includes("/") ||
-      calcDisplayValue.includes("*")
-    ) {
+    if (checkForOperators(calcDisplay)) {
       adjustingAttributes(item);
     } else {
       calcDisplay.textContent += item.value;
-      calcDisplayValue = calcDisplay.textContent;
       item.setAttribute("disabled", "");
     }
   });
 });
 
+function checkForOperators(string) {
+  if (
+    string.textContent.includes("+") ||
+    string.textContent.includes("-") ||
+    string.textContent.includes("/") ||
+    string.textContent.includes("*")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 function adjustingAttributes(button) {
   operators.forEach((item) => {
     if (item === button) {
       item.setAttribute("disabled", "");
-      calcDisplay.textContent = calcDisplayValue.replace(
+      let replacedString = calcDisplay.textContent.replace(
         /\*|\/|\+|\-/,
         button.value
       );
+      calcDisplay.textContent = replacedString;
     } else {
       item.removeAttribute("disabled");
     }
   });
 }
 
-
 function operate(displayValue) {
+  console.log(displayValue)
   if (!displayValue) return;
   operators.forEach((item) => item.removeAttribute("disabled"));
   const slicingIdx = displayValue
@@ -75,7 +83,6 @@ function operate(displayValue) {
     let operator = displayValue.slice(slicingIdx, slicingIdx + 1);
     let secondNumber = displayValue.slice(slicingIdx + 1);
     if (secondNumber) {
-
       switch (operator) {
         case "+":
           calcDisplay.textContent = add(firstNumber, secondNumber);
@@ -101,7 +108,6 @@ function operate(displayValue) {
     return;
   }
 }
-
 
 function add(num, num2) {
   return Number(num) + Number(num2);
