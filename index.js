a16cd8e02cde6575854d6c686caf0c0ff6662cc7
Alex <aamorgun2003@gmail.com>
2024-01-02 18:05:32 -0700

    Update divide function
    Changed the divide function to check for '0' or '.' with the
    every method instead of using findIndex.

--- index.js
+++ index.js
@@ -199,8 +199,8 @@ function multiply(num, num2) {
 }
 
 function divide(num, num2) {
-  let num2Iterate = num2.split("").findIndex((item) => item === "0" || item === '.');
-  if (num2 === "0" || num2[num2Iterate] !== '0' || num2[num2Iterate] !== '.') {
+  let num2Iterate = num2.split("").every(item => item === '0' || item === '.');
+  if (num2Iterate) {
     alert("Can't divide by zero!");
     return "0";
   } else {
